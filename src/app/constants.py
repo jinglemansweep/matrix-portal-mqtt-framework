6776d0e55f309b6d27dc059b1bab7f38dc6c6751
Louis King <jinglemansweep@gmail.com>
2022-11-20 14:24:58 +0000

    Tidying

--- src/app/constants.py
+++ src/app/constants.py
@@ -6,12 +6,12 @@ NETWORK_ENABLE = secrets.get("network_enable", True)
 NTP_ENABLE = secrets.get("ntp_enable", True)
 MQTT_ENABLE = secrets.get("mqtt_enable", True)
 HASS_ENABLE = secrets.get("hass_enable", True)
-NTP_INTERVAL = secrets.get("ntp_interval", 60)
+NTP_INTERVAL = secrets.get("ntp_interval", 60 * 60 * 12)
 MATRIX_WIDTH = secrets.get("matrix_width", 64)
 MATRIX_HEIGHT = secrets.get("matrix_height", 32)
 MATRIX_BIT_DEPTH = secrets.get("matrix_bit_depth", 5)
 MATRIX_COLOR_ORDER = secrets.get("matrix_color_order", "RGB")
-MQTT_PREFIX = secrets.get("mqtt_prefix", "mqticker")
+MQTT_PREFIX = secrets.get("mqtt_prefix", "mpmqtt")
 
 # CONSTANTS
 _ASYNCIO_DELAY = 0.01
